8b7f67b98556b795e8b3531383462bdad178d315
Vladimir Zhuravlev <private.face@gmail.com>
2012-01-06 19:44:05 +0400

    jquery 1.7 support

--- js/jquery.bind-first.js
+++ js/jquery.bind-first.js
@@ -1,25 +1,49 @@
 (function($) {
-	var JQ_VERSION = parseFloat($().version);
-
-	function bindFirst($el, eventName, args) {
-		args.unshift(eventName);
-		$.fn.bind.apply($el, args);
-		var pureEventName = $.trim(eventName).match(/[^\.]+/i)[0];
-		var events = $el.data('events')[pureEventName];
-		events.unshift(events.pop());
+	var JQ_VERSION = parseFloat($.fn.jquery);
+	
+	function eventsData($el, key, value) {
+		var data = JQ_VERSION < 1.7 ? $el.data('events') : $._data($el[0]).events;
+		if (!key || !value) {
+			return data;
+		}
+		data[key] = value;
 	}
+	
+	function moveHandlerToTop($el, eventName, isDelegated) {
+		var data = eventsData($el);
+		var events = data[eventName];
+		
+		if (JQ_VERSION < 1.7) {
+			if (isDelegated) {
+				data.live.unshift(data.live.pop());
+			} else {
+				events.unshift(events.pop());
+			}
+			return;
+		}
 
+		var handler = events.pop();
+		events.splice(isDelegated ? 0 : (events.delegateCount || 0), 0, handler);
+	}
+	
+	function moveEventHandlers($elems, eventsString, isDelegate) {
+		var events = eventsString.split(/\s+/);
+		$elems.each(function() {
+			for (var i = 0; i < events.length; ++i) {
+				var pureEventName = $.trim(events[i]).match(/[^\.]+/i)[0];
+				moveHandlerToTop($(this), pureEventName, isDelegate);
+			}
+		});
+	}
+	
 	$.fn.bindFirst = function() {
+		var $el = $(this);
 		var args = $.makeArray(arguments);
 		var eventsString = args.shift();
+
 		if (eventsString) {
-			$(this).each(function() {
-				var $el = $(this);
-				var events = eventsString.split(/\s+/);
-				for (var i = 0; i < events.length; ++i) {
-					bindFirst($el, events[i], args);
-				}
-			});
+			$.fn.bind.apply($el, arguments);
+			moveEventHandlers($el, eventsString);
 		}
 
 		return $(this);
@@ -28,26 +52,23 @@
 	$.fn.delegateFirst = function() {
 		var $el = $(this);
 		var args = $.makeArray(arguments);
-		var l = (args[1] || '').split(/\s+/).length;
-		$.fn.delegate.apply($el, args);
+		var eventsString = args[1];
 		
-		$el.each(function() {
-			var $el = $(this);
-			var events = $el.data('events').live;
-			events = events.splice(-1 * l, l).concat(events);
-			$el.data('events').live = events;
-			console.log(events);
-		});
-		
+		if (eventsString) {
+			args.splice(0, 2);
+			$.fn.delegate.apply($el, arguments);
+			moveEventHandlers($(this), eventsString, true);
+		}
+
 		return $el;
 	};
 
 	$.fn.liveFirst = function() {
 		var $el = $(this);
 		var args = $.makeArray(arguments);
-		args.unshift($el.selector);
 
-		// bind live event(s)
+		// live = delegate to document
+		args.unshift($el.selector);
 		$.fn.delegateFirst.apply($(document), args);
 
 		return $el;

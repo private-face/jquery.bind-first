315738717d49aa5e5814e7132f5503392afc09b8
Vladimir Zhuravlev <private.face@gmail.com>
2012-01-06 18:04:36 +0400

    spaces to tabs

--- js/jquery.bind-first.js
+++ js/jquery.bind-first.js
@@ -1,34 +1,34 @@
 (function($) {
 	var JQ_VERSION = parseFloat($().version);
 
-    function bindFirst($el, eventName, args) {
-        args.unshift(eventName);
-        $.fn.bind.apply($el, args);
-        var pureEventName = $.trim(eventName).match(/[^\.]+/i)[0];
-        var events = $el.data('events')[pureEventName];
-        events.unshift(events.pop());
-    }
+	function bindFirst($el, eventName, args) {
+		args.unshift(eventName);
+		$.fn.bind.apply($el, args);
+		var pureEventName = $.trim(eventName).match(/[^\.]+/i)[0];
+		var events = $el.data('events')[pureEventName];
+		events.unshift(events.pop());
+	}
 
-    $.fn.bindFirst = function() {
-        var args = $.makeArray(arguments);
-        var eventsString = args.shift();
-        if (eventsString) {
-            $(this).each(function() {
-                var $el = $(this);
-                var events = eventsString.split(/\s+/);
-                for (var i = 0; i < events.length; ++i) {
-                    bindFirst($el, events[i], args);
-                }
-            });
-        }
+	$.fn.bindFirst = function() {
+		var args = $.makeArray(arguments);
+		var eventsString = args.shift();
+		if (eventsString) {
+			$(this).each(function() {
+				var $el = $(this);
+				var events = eventsString.split(/\s+/);
+				for (var i = 0; i < events.length; ++i) {
+					bindFirst($el, events[i], args);
+				}
+			});
+		}
 
-        return $(this);
-    };
+		return $(this);
+	};
 
 	$.fn.delegateFirst = function() {
 		var $el = $(this);
-        var args = $.makeArray(arguments);
-        var l = (args[1] || '').split(/\s+/).length;
+		var args = $.makeArray(arguments);
+		var l = (args[1] || '').split(/\s+/).length;
 		$.fn.delegate.apply($el, args);
 		
 		$el.each(function() {
@@ -42,15 +42,15 @@
 		return $el;
 	};
 
-    $.fn.liveFirst = function() {
-        var $el = $(this);
-        var args = $.makeArray(arguments);
-        args.unshift($el.selector);
+	$.fn.liveFirst = function() {
+		var $el = $(this);
+		var args = $.makeArray(arguments);
+		args.unshift($el.selector);
 
-        // bind live event(s)
-        $.fn.delegateFirst.apply($(document), args);
+		// bind live event(s)
+		$.fn.delegateFirst.apply($(document), args);
 
-        return $el;
-    };
+		return $el;
+	};
 
 })(jQuery);
